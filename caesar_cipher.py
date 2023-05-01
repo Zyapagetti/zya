e2f10abf19300ac281e0cf669e88b6c3058c31f9
zyapagetti <jeff9435@gmail.com>
2023-05-01 18:47:20 +0900

    feat: 갑자기 신나서 커밋도 안하고 다 해버림 ㅎ

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,5 +1,35 @@
 import string
 
-arr = list(string.ascii_uppercase)
+step = int(input("몇단계?: "))%26
+quiz = input("문장을입력: ").upper()
+print("Input: " + quiz)
 
+arr = list(string.ascii_uppercase)
+print("AtoZ array:")
 print(arr)
+
+key = []
+result = []
+
+for i in range(len(arr)):
+    if i < step:
+        key.append(arr[i+len(arr)-step])
+    else:
+        key.append(arr[i-step])
+
+print("encryption key:")
+print(key)
+
+arr_quiz = list(quiz)
+
+print("Input array:")
+print(arr_quiz)
+
+for i in range(len(arr_quiz)):
+    ind = arr.index(arr_quiz[i])
+    result.append(key[ind])
+
+print("Output array:")
+print(result)
+
+print("Output: " + ''.join(result))
